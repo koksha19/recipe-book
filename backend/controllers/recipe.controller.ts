1af167da78a25192cf0d385e6bc32f493662abde
Lev <lev.bereza@gmail.com>
2025-04-14 20:14:54 +0300

    feat: add middleware to catch errors

--- backend/controllers/recipe.controller.ts
+++ backend/controllers/recipe.controller.ts
@@ -1,35 +1,28 @@
 import { Request, Response } from 'express';
 import recipeService from '../services/recipe.service';
+import { catchError } from '../middleware/catch-error.middleware';
 
-export const getRecipes = async (req: Request, res: Response) => {
+const getRecipes = catchError(async (req: Request, res: Response) => {
+  const { ingredient, country, category } = req.query;
   let data;
 
-  try {
-    const { ingredient, country, category } = req.query;
-    console.log(ingredient, country, category);
-
-    if (ingredient) {
-      data = await recipeService.filterByIngredient(ingredient as string);
-    } else if (country) {
-      data = await recipeService.filterByCountry(country as string);
-    } else if (category) {
-      data = await recipeService.filterByCategory(category as string);
-    } else {
-      data = await recipeService.fetchAll();
-    }
-
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch recipes', error });
+  if (ingredient) {
+    data = await recipeService.filterByIngredient(ingredient as string);
+  } else if (country) {
+    data = await recipeService.filterByCountry(country as string);
+  } else if (category) {
+    data = await recipeService.filterByCategory(category as string);
+  } else {
+    data = await recipeService.fetchAll();
   }
-};
 
-export const getRecipeInfo = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const data = await recipeService.fetchInfo(id);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch recipe details', error });
-  }
-};
+  res.json(data);
+});
+
+const getRecipeInfo = catchError(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = await recipeService.fetchInfo(id);
+  res.json(data);
+});
+
+export { getRecipes, getRecipeInfo };
--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 
 import recipeRoutes from './routes/recipe.routes';
@@ -11,6 +11,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', recipeRoutes);
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, statusCode: status, data: data });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
